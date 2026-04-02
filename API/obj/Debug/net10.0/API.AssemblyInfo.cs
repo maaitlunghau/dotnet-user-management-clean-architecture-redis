24a18d8d1ca0ea3fa80ef1d220cda2af13ca7dc1
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 22:29:51 +0700

    feat(api): configure SwaggerUI for testing API

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a4b4fa6ab4eda8d85f92e5ef01ecc6fc7abaa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e1ec208cd95f246ce6e15c93dc1351c08c75d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
