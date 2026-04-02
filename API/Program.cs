cbcedbff863b37e8c22ecd37e9aa00717d35d559
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 22:31:21 +0700

    feat(api): configure auto redirect to /swagger when server run

--- API/Program.cs
+++ API/Program.cs
@@ -22,4 +22,6 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+
 app.Run();
--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e1ec208cd95f246ce6e15c93dc1351c08c75d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a18d8d1ca0ea3fa80ef1d220cda2af13ca7dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
