6f23c01d154954f265d7d2c0ded31993f25434a8
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 21:23:26 +0700

    feat(api): add reference to application and infras project

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea4a8593517e71b770a8828a6c85cd9df05d664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de4542d51fd0befadace772aa3eb9fcf2e6feda")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
