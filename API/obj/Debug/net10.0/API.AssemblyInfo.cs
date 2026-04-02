43a4b4fa6ab4eda8d85f92e5ef01ecc6fc7abaa6
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 22:17:11 +0700

    feat: add initial database migrations and setup schema

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5fa7470bfe08bb29bedf69224c46ac9c146ca4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a9767a8440890c5b87204502b5ab295c94e897")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- INFRASTRUCTURE/obj/Debug/net10.0/INFRASTRUCTURE.AssemblyInfo.cs
+++ INFRASTRUCTURE/obj/Debug/net10.0/INFRASTRUCTURE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFRASTRUCTURE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96dce3cad5061b21a833caf3643b843a56b1e4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a9767a8440890c5b87204502b5ab295c94e897")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFRASTRUCTURE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFRASTRUCTURE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
