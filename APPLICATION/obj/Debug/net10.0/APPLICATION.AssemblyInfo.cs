ba26f3dbe10cb8ae4fdb4cc0c299103b2eb6869e
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 21:57:01 +0700

    chore(application): install DependencyInjection.Abstractions package

--- APPLICATION/obj/Debug/net10.0/APPLICATION.AssemblyInfo.cs
+++ APPLICATION/obj/Debug/net10.0/APPLICATION.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APPLICATION")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f23c01d154954f265d7d2c0ded31993f25434a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6252a1236ce3e51a1398f1a7c6468ba2dafac4f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("APPLICATION")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APPLICATION")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
